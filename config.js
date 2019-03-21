4bfc65019ce10458066eb586b6c77d5b7584842e
Caitlin MacKay <Caitlin@MacBook-Air.fc.ca>
2019-03-21 17:04:09 -0400

    homework

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    user: "root",
+    password: "root",
+    port: 8889, 
+    host: "localhost",
+    database: "marvelcharacters"
+ }
+
+ module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
+var connect = require('../utils/sqlConnect');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //get date from the db, and then send it through the route
+  //to the views
+  connect.query(`SELECT name, avatar FROM hero`, (err, result) => {
+    if (err) {
+      throw err;
+      console.log(err);
+    } else {
+      console.log(result);
+      res.render('index', { avatars: result});
+    }
+  });
 });
+
+//get one heros bio info
+
+
+router.get('/:hero', function(req, res, next) {
+  //get date from the db, and then send it through the route
+  //to the views
+  connect.query(`SELECT * FROM hero WHERE name="${req.params.hero}"`, (err, result) => {
+    if (err) {
+      throw err;
+      console.log(err);
+    } else {
+      console.log(result);
+      res.render('bio', { bioData: result[0]});
+    }
+  });
+});
+
 
 module.exports = router;
